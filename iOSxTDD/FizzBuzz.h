177a600e790da12620a4b2459a503c4f9c089390
akuraru <akuraru@gmail.com>
2014-01-31 22:29:03 +0900

    クラスを追加

---
+++ iOSxTDD/FizzBuzz.h
@@ -0,0 +1,13 @@
+//
+//  FizzBuzz.h
+//  iOSxTDD
+//
+//  Created by akuraru on 2014/01/31.
+//  Copyright (c) 2014年 akuraru. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FizzBuzz : NSObject
+
+@end
---
+++ iOSxTDD/FizzBuzz.m
@@ -0,0 +1,13 @@
+//
+//  FizzBuzz.m
+//  iOSxTDD
+//
+//  Created by akuraru on 2014/01/31.
+//  Copyright (c) 2014年 akuraru. All rights reserved.
+//
+
+#import "FizzBuzz.h"
+
+@implementation FizzBuzz
+
+@end
