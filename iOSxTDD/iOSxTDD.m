302dec5fc43e37f081695073164ed96160eee983
akuraru <akuraru@gmail.com>
2014-02-01 15:50:56 +0900

    テストを追加する

--- iOSxTDD/iOSxTDD.m
+++ iOSxTDD/iOSxTDD.m
@@ -21,4 +21,22 @@
     XCTAssertEqualObjects(result, @"1", @"1を送ると文字列の1を返す");
 }
 
+- (void)test3 {
+    FizzBuzz *fizzBuzz = [[FizzBuzz alloc] init];
+    NSString *result = [fizzBuzz at:3];
+    XCTAssertEqualObjects(result, @"Fizz", @"3を送ると文字列のFizzを返す");
+}
+
+- (void)test5 {
+    FizzBuzz *fizzBuzz = [[FizzBuzz alloc] init];
+    NSString *result = [fizzBuzz at:5];
+    XCTAssertEqualObjects(result, @"Buzz", @"5を送ると文字列のBuzzを返す");
+}
+
+- (void)test15 {
+    FizzBuzz *fizzBuzz = [[FizzBuzz alloc] init];
+    NSString *result = [fizzBuzz at:15];
+    XCTAssertEqualObjects(result, @"FizzBuzz", @"15を送ると文字列のFizzBuzzを返す");
+}
+
 @end
