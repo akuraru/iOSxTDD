17600ae58437525842ad528a5d26933d151c3352
akuraru <akuraru@gmail.com>
2014-02-02 13:51:06 +0900

    テストファイルを作る

---
+++ iOSxTDD/FizzBuzzSpec.m
@@ -0,0 +1,12 @@
+//
+//  FizzBuzzSpec.m
+//  iOSxTDD
+//
+//  Created by akuraru on 2014/02/02.
+//  Copyright (c) 2014年 akuraru. All rights reserved.
+//
+
+#import "Kiwi.h"
+
+SPEC_BEGIN(FizzBuzzSpec)
+SPEC_END
