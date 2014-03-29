ac8f09e4998c0d648480e5068f73956c3b810677
akuraru <akuraru@gmail.com>
2014-03-29 18:33:39 +0900

    contextを積むことで任意の状態を作る。

--- iOSxTDDTests/CoreDataSpec.m
+++ iOSxTDDTests/CoreDataSpec.m
@@ -1,5 +1,6 @@
 #import "Kiwi.h"
 #import "CoreData+MagicalRecord.h"
+#import "Item.h"
 
 SPEC_BEGIN(CoreDataSpec)
 describe(@"CoreData", ^{
@@ -9,6 +10,35 @@ describe(@"CoreData", ^{
     });
     afterEach(^{
         [MagicalRecord cleanUp];
+    });
+    context(@"create item", ^{
+        __block Item *item;
+        beforeEach(^{
+            item = [Item MR_createEntity];
+            [item.managedObjectContext MR_saveOnlySelfAndWait];
+        });
+        it(@"count", ^{
+            [[theValue([Item MR_countOfEntities]) should] equal:theValue(1)];
+        });
+        context(@"delete item", ^{
+            beforeEach(^{
+                [item MR_deleteEntity];
+                [item.managedObjectContext MR_saveOnlySelfAndWait];
+            });
+            it(@"count", ^{
+                [[theValue([Item MR_countOfEntities]) should] equal:theValue(0)];
+            });
+        });
+        context(@"more create item", ^{
+            __block Item *anotherItme;
+            beforeEach(^{
+                anotherItme = [Item MR_createEntity];
+                [anotherItme.managedObjectContext MR_saveOnlySelfAndWait];
+            });
+            it(@"count", ^{
+                [[theValue([Item MR_countOfEntities]) should] equal:theValue(2)];
+            });
+        });
     });
 });
 SPEC_END
